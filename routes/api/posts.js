e589e2f1b6b44d34702e4568f2dc3632d4b62a8a
aharri64 <haveacompleteday@gmail.com>
2021-03-14 04:59:05 -0600

    add create a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,7 +10,34 @@ const checkObjectId = require('../../middleware/checkObjectId');
 //* route:  POST api/posts
 //? desc:   Create a post
 //! access: Private
-router.get('/', (req, res) => res.send('Post route'))
+router.post('/', auth,
+    check('text', 'Text is required').notEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+  
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+    
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+    
+            const post = await newPost.save();
+    
+            res.json(post);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
 
 
 //* route:  GET api/posts
