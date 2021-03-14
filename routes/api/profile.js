a9ee7207847f7886995207e30e38070f8fbec939
aharri64 <haveacompleteday@gmail.com>
2021-03-14 03:01:56 -0600

    profile route: add Create or update user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,6 +13,7 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 // const Post = require('../../models/Post');
 
+
 //* route:  Get api/profile/me
 //? desc:   Get current users profile
 //! access: Private
@@ -32,4 +33,72 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+
+//* route:  POST api/profile
+//? desc:   Create or update user profile
+//! access: Private
+router.post('/', auth,
+    check('currentProject', 'currentProject is required').notEmpty(), //! might not work
+    check('skills', 'Skills is required').notEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+    //# destructure the request
+        const {
+            website,
+            skills,
+            youtube,
+            twitter,
+            instagram,
+            linkedin,
+            facebook,
+            // spread the rest of the fields we don't need to check
+            ...rest
+        } = req.body;
+
+    //# build a profile
+        const profileFields = {
+            user: req.user.id,
+            website:
+                website && website !== ''
+                    ? normalize(website, { forceHttps: true })
+                    : '',
+            skills: Array.isArray(skills)
+                ? skills
+                : skills.split(',').map((skill) => ' ' + skill.trim()),
+            ...rest
+        };
+
+    //# Build socialFields object
+        const socialFields = { youtube, twitter, instagram, linkedin, facebook };
+
+    //# normalize social fields to ensure valid url
+        for (const [key, value] of Object.entries(socialFields)) {
+            if (value && value.length > 0)
+                socialFields[key] = normalize(value, { forceHttps: true });
+        }
+    //# add to profileFields
+        profileFields.social = socialFields;
+
+        try {
+    //# Using upsert option (creates new doc if no match is found):
+            let profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true, upsert: true, setDefaultsOnInsert: true }
+            );
+            return res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send('Server Error');
+        }
+    }
+);
+
+
+
 module.exports = router;
