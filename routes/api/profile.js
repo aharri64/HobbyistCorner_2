1addf7127f75006dd428ff62069f6f9860a1f6e1
aharri64 <haveacompleteday@gmail.com>
2021-03-14 00:47:57 -0700

    Get current users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const axios = require('axios');
+const config = require('config');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+//? bring in normalize to give us a proper url, regardless of what user entered
+const normalize = require('normalize-url');
+// const checkObjectId = require('../../middleware/checkObjectId');
 
-//* route:  Get api/profile
-//? desc:   Test Route
-//! access: Public
-router.get('/', (req, res) => res.send('Profile route'))
+//* Database
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+// const Post = require('../../models/Post');
 
+//* route:  Get api/profile/me
+//? desc:   Get current users profile
+//! access: Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'avatar']);
+    //? .populate brings in attributes from the referenced model
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+    
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
