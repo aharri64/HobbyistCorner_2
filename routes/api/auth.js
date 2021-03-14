5f475123d0f7acd65f2ed5f5b4b2241c3105165d
aharri64 <haveacompleteday@gmail.com>
2021-03-13 23:30:42 -0700

    add get user by token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const auth = require('../../middleware/auth'); //? auth middleware
+const jwt = require('jsonwebtoken');
+const config = require('config'); //? access to config
+const { check, validationResult } = require('express-validator');
 
-//* route:  Get api/auth
+//* Database
+const User = require('../../models/User');
+
+//# route:  Get api/auth/test
 //? desc:   Test Route
 //! access: Public
-router.get('/', (req, res) => res.send('Auth route'))
+router.get('/test', (req, res) => res.send('Auth route'))
+
+
+//# route:  Get api/auth
+//? desc:   Get user by token
+//! access: Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');//? -password will leave off pasword in data
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 module.exports = router;
