85e2b069a483a427ba7ae266c42f31b4d0e92f1c
aharri64 <haveacompleteday@gmail.com>
2021-03-14 10:10:02 -0600

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
 
 // Redux
 import { Provider } from 'react-redux';
@@ -17,6 +19,7 @@ const App = () => {
         <Fragment>
           <Navbar />
           <Route exact path="/" component={Landing} />
+          
           <section className="container">
             <switch>
               <Route exact path="/login" component={Login} />
