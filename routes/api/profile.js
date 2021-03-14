a520c026d5578d83e0bd625e1a17ebef2ae4d9d3
aharri64 <haveacompleteday@gmail.com>
2021-03-14 03:23:36 -0600

    profile route: add delete route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -133,4 +133,26 @@ router.get('/user/:user_id', checkObjectId('user_id'),
         }
     }
 );
+
+
+//* route:  DELETE api/profile
+//? desc:   Delete profile, user & posts
+//! access: Private
+router.delete('/', auth, async (req, res) => {
+    try {
+    // Remove user posts
+    // Remove profile
+    // Remove user
+        await Promise.all([
+            Post.deleteMany({ user: req.user.id }),
+            Profile.findOneAndRemove({ user: req.user.id }),
+            User.findOneAndRemove({ _id: req.user.id })
+        ]);
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
