dd7a91bc3f848dbd2209829a0539ceea893a9ff2
aharri64 <haveacompleteday@gmail.com>
2021-03-14 04:59:56 -0600

    add get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -17,7 +17,7 @@ router.post('/', auth,
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-  
+
         try {
             const user = await User.findById(req.user.id).select('-password');
     
@@ -39,11 +39,18 @@ router.post('/', auth,
 );
 
 
-
 //* route:  GET api/posts
 //? desc:   Get all posts
 //! access: Private
-
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 //* route:  GET api/posts/:id
 //? desc:   Get post by ID
